8c155e1d27138b4df67412ce026c64ab3ba73a7d
Junxiao Shi <git@mail1.yoursunny.com>
2016-12-24 11:08:53 -0700

    connect to WiFi and Losant

--- LCD.ino
+++ LCD.ino
@@ -1,11 +1,51 @@
+#include <ESP8266WiFi.h>
 #include <LiquidCrystal_I2C.h>
+#include <Losant.h>
+#include <Streaming.h>
+#include "credentials.hpp"
 
 LiquidCrystal_I2C lcd(0x3F, 16, 2);
+
+WiFiClientSecure losantSocket;
+LosantDevice losantDevice(LOSANT_DEVICE_ID);
+
+void
+connect()
+{
+  if (WiFi.status() == WL_CONNECTED && losantDevice.connected()) {
+    return;
+  }
+
+  Serial << "Connecting to WiFi ";
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial << WiFi.status();
+  }
+  Serial << endl << "Connected to WiFi " << WiFi.localIP() << endl;
+
+  Serial << "Connecting to Losant ";
+  losantDevice.connectSecure(losantSocket, LOSANT_ACCESS_KEY, LOSANT_ACCESS_SECRET);
+  while (!losantDevice.connected()) {
+    delay(1000);
+    Serial << '.';
+  }
+  Serial << endl << "Connected to Losant" << endl;
+}
 
 void
 setup()
 {
-  Wire.pins(12, 14);
+  Wire.pins(12, 14); // D5-SCL, D6-SDA
+
+  Serial.begin(115200);
+  Serial.println();
+  Serial.println();
+
+  WiFi.disconnect();
+  WiFi.persistent(false);
+  WiFi.mode(WIFI_STA);
+  WiFi.setSleepMode(WIFI_MODEM_SLEEP);
 
   lcd.begin(16,2);
   lcd.init();
@@ -22,4 +62,6 @@ setup()
 void
 loop()
 {
+  connect();
+  losantDevice.loop();
 }
---
+++ credentials.sample.hpp
@@ -0,0 +1,11 @@
+#ifndef CREDENTIALS_HPP
+#define CREDENTIALS_HPP
+
+const char* WIFI_SSID = "my-wifi-ssid";
+const char* WIFI_PASS = "my-wifi-pass";
+
+const char* LOSANT_DEVICE_ID = "x";
+const char* LOSANT_ACCESS_KEY = "x";
+const char* LOSANT_ACCESS_SECRET = "x";
+
+#endif // CREDENTIALS_HPP
